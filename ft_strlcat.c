e9a06b2ddc6107d7e85d39960ea7430e112d6656
Gracia Del Castillo Montull <gdel-cas@c2r13s3.42madrid.com>
2023-09-27 14:41:53 +0200

    algo habre añadido

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: gdel-cas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/25 16:48:20 by gdel-cas          #+#    #+#             */
-/*   Updated: 2023/09/25 18:18:28 by gdel-cas         ###   ########.fr       */
+/*   Updated: 2023/09/25 19:21:42 by gdel-cas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include<stdio.h>
@@ -18,24 +18,24 @@ size_t	ft_strlcat(char * dst, const char * src, size_t dstsize)
 
 	i = 0;
 	y = 0;
-	if (dstsize > 0)
+	if (dstsize >= 0)
 	{	
 		while(dst[i] != '\0')
 			i++;
-		while (src[y] && i <= (dstsize))
+		while (src[y] != 0 && y < (dstsize - i - 1))
 		{
-			dst[i] = src[y];
+			dst[i + y] = src[y];
 			y++;
-		}
+		}	
 	}
 return(i + y);
 }
 int	main()
 {
-	char	c[12] = "HOLA";
+	char	c[20] = "HOLA";
 	char	b[] = "buenas";
-	printf("%zu\n", ft_strlcat(c, b, 8));
-	printf("%zu\n", strlcat(c, b, 8));
+	printf("%zu\n", strlcat(c, b, 11));
+	//printf("%zu\n", ft_strlcat(c, b, 10i));
 	printf("%s\n", c);
 	return (0);
 }
