090cd65e8eea1a0c91145adce7aad3e3c9a39192
Gracia Del Castillo Montull <gdel-cas@c2r12s2.42madrid.com>
2023-09-28 17:49:00 +0200

    cositas

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,21 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft.strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gdel-cas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 14:46:58 by gdel-cas          #+#    #+#             */
-/*   Updated: 2023/09/27 15:38:46 by gdel-cas         ###   ########.fr       */
+/*   Updated: 2023/09/28 16:55:53 by gdel-cas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include<stdio.h>
 
-char	*strchr(const char *s, int c)
+char	*ft_strchr(const char *s, int c)
 {
 	int	i;
 	
 	i = 0;
 	if (s[i] == '\0')
-		return('\0');
-	while (s[i] == (char)c)
+		return(NULL);
+	if (s[i] == (char)c)
+		return(c);
+}
+int	main()
+{
+	char	c[10] = "hola";
+	printf("%c\n", *ft_strchr(c[2], 10 ));
 }
