bc928ed33eb3d2636c3b14a01a76824d449056b7
Grc96 <138579075+Grc96@users.noreply.github.com>
2023-10-03 17:39:40 +0200

    Add files via upload

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -5,9 +5,51 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: gdel-cas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/02 20:08:22 by gdel-cas          #+#    #+#             */
-/*   Updated: 2023/10/02 20:10:12 by gdel-cas         ###   ########.fr       */
+/*   Created: 2023/10/03 14:35:52 by gdel-cas          #+#    #+#             */
+/*   Updated: 2023/10/03 17:33:55 by gdel-cas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-  char	*strnstr(const char *big, const char *little, size_t len)
+#include <string.h>
+#include <stdio.h>
+#include "libft.h"
+
+char	*ft_strnstr(const char *hay, const char *need, size_t n)
+{
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	if ((char)need[j] == '\0')
+		return ((char *)hay);
+	if (hay == 0 || ft_strlen(need) > n)
+		return (0);
+	while ((char)hay[i] != '\0')
+	{
+		j = 0;
+		while ((char)hay[i + j] == (char)need[j] && i + j < n)
+		{
+			j++;
+		}
+		if ((char)need[j] == '\0')
+			return ((char *)hay + i);
+		i++;
+	}
+	return (0);
+}
+
+/*
+int	main()
+{
+    char    s1[] = "aaabcabcd";
+    char    s2[] = "cdjghyffdgfgfd";
+    size_t  max = 3;
+
+    char    *i1 = strnstr(s1, s2, max);
+    char    *i2 = ft_strnstr(s1, s2, max);
+
+
+    printf("strnstr: %s\nft_strnstr: %s\n", i1,i2);
+    return 0;
+}*/
